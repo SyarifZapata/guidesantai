6377ea2c9b76707d5f4bd2592908cae2a6251977
syarif <syarif.hidayatullah@stud.unibas.ch>
2018-07-31 18:53:15 +0200

    added router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { UebungenComponent } from './uebungen/uebungen.component';
+import { AboutComponent } from './about/about.component';
+import { RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'uebungen', component: UebungenComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    UebungenComponent
+    UebungenComponent,
+    AboutComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
