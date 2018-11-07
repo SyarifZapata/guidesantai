78656e708d1d8d792883fa7e95e2d5efd165aafe
syarif <syarif.hidayatullah@stud.unibas.ch>
2018-11-07 20:17:40 +0100

    added Jquery and materialize css into node module

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {DataService} from '../data.service';
 import {Router} from '@angular/router';
+import * as $ from 'jquery';
+import * as M from 'materialize-css';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, AfterViewInit{
   // We use this value to show logout and memberpage button on the navbar
   loggedIn: boolean;
 
   constructor(private _dataService: DataService, private _router: Router) { }
+
+  /* jquery initalization doesnt work, uses vanila js instead */
+  ngAfterViewInit(){
+    // $(document).ready(() => {
+    //   $('.dropdown-trigger').dropdown();
+    // });
+
+    document.addEventListener('DOMContentLoaded', function() {
+      const elems = document.querySelectorAll('.dropdown-trigger');
+      const instances = M.Dropdown.init(elems, []);
+    });
+  }
 
   ngOnInit() {
     // The value of logged in always get updated whenever it is changed by other component.
